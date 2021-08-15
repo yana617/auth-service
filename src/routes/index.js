2357429b8a1e14f86beb8d6fc5838d08211d8e76
Yana <jana.ru.sidorova@yandex.ru>
2021-08-16 00:31:31 +0300

    Implement additional field templates logic

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const authRoute = require('./auth');
 const usersRoute = require('./users');
+const aftRoute = require('./additional-field-templates');
 
 router.use('/auth', authRoute);
 router.use('/users', usersRoute);
+router.use('/additional-field-templates', aftRoute);
 
 module.exports = router;
