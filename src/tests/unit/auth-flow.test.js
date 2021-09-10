5d7d1694bf81987bd7f7260a35f89a6ca3d7b6fb
Yana <jana.ru.sidorova@yandex.ru>
2021-09-10 17:24:06 +0300

    Implement additional tests

--- src/tests/unit/auth-flow.test.js
+++ src/tests/unit/auth-flow.test.js
@@ -1,13 +1,18 @@
 const request = require('supertest');
+const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 
 const app = require('../../../app');
 const db = require('../../database');
 const { generateUser } = require('../fixtures/db');
 
+const { BCRYPT_SALT: bcryptSalt } = process.env;
+
 beforeEach(async () => {
   await db.User.destroy({ where: {} });
   await db.AdditionalFieldTemplate.destroy({ where: {} });
   await db.UserAdditionalField.destroy({ where: {} });
+  await db.Token.destroy({ where: {} });
 });
 
 test('I can register, login and get own info successfully', async () => {
@@ -41,4 +46,59 @@ test('I can register, login and get own info successfully', async () => {
   const { data: user } = response.body;
   expect(user).toBeDefined();
   expect(user.name).toEqual(userToSave.name);
+});
+
+const createToken = async (userId) => {
+  await db.Token.destroy({ where: {} });
+  const resetToken = crypto.randomBytes(32).toString('hex');
+  const hash = await bcrypt.hash(resetToken, Number(bcryptSalt));
+  await db.Token.create({ user_id: userId, token: hash });
+  return resetToken;
+};
+
+test('I forgot password and can reset it successfully', async () => {
+  const userOne = generateUser();
+  const registerResponse = await request(app)
+    .post('/auth/register')
+    .send(userOne)
+    .expect(201);
+
+  const { data: newUser } = registerResponse.body;
+  expect(newUser.name).toEqual(userOne.name);
+  const userInDb = await db.User.findOne({ where: { phone: userOne.phone } });
+  expect(userInDb).toBeDefined();
+  expect(userInDb.name).toEqual(userOne.name);
+
+  const updatedPassword = '111111';
+  const forgotResponse = await request(app)
+    .post('/auth/forgot-password')
+    .send({ email: userOne.email })
+    .expect(200);
+
+  const { success } = forgotResponse.body;
+  expect(success).toBeTruthy();
+
+  const token = await createToken(userInDb.id);
+
+  const resetResponse = await request(app)
+    .post('/auth/reset-password')
+    .send({
+      token,
+      userId: userInDb.id,
+      password: updatedPassword,
+    })
+    .expect(200);
+
+  const { success: resetSuccess } = resetResponse.body;
+  expect(resetSuccess).toBeTruthy();
+
+  const loginResponse = await request(app)
+    .post('/auth/login')
+    .send({ email: userOne.email, password: updatedPassword })
+    .expect(200);
+
+  const { data: loginUser } = loginResponse.body;
+
+  expect(loginUser.name).toEqual(userOne.name);
+  expect(loginUser.token).not.toBeNull();
 });
