5d765badeb9ea54414ef9cf1c7e120ae72608b79
Yana <jana.ru.sidorova@yandex.ru>
2021-10-08 02:10:17 +0300

    Fix aft errors translates

--- src/middlewares/validations/validateAdditionalFieldTemplate.js
+++ src/middlewares/validations/validateAdditionalFieldTemplate.js
@@ -1,20 +1,20 @@
 const { checkSchema } = require('express-validator');
 
+const { ERRORS } = require('../../translations');
+
 module.exports = checkSchema({
   label: {
     in: ['body'],
-    isString: true,
     isLength: {
-      errorMessage: 'Label should be from 2 to 30 characters',
+      errorMessage: ERRORS.AFT_LABEL_ERROR,
       options: { min: 2, max: 30 },
     },
     exists: true,
   },
   description: {
     in: ['body'],
-    isString: true,
     isLength: {
-      errorMessage: 'Description should be from 50 to 150 characters',
+      errorMessage: ERRORS.AFT_DESCRIPTION_ERROR,
       options: { min: 50, max: 150 },
     },
     exists: true,
@@ -22,7 +22,7 @@ module.exports = checkSchema({
   icon: {
     in: ['body'],
     isLength: {
-      errorMessage: 'Icon should be from 10 to 150 characters',
+      errorMessage: ERRORS.AFT_ICON_ERROR,
       options: { min: 10, max: 150 },
     },
     optional: { options: { nullable: true } },
